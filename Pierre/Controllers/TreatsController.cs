ae21d2bb2e936a555d8ec4f9df0cb717c0ed5499
Sharon Lee <sharonlee714@gmail.com>
2020-01-24 14:59:18 -0800

    add index and details to above

--- Pierre/Controllers/TreatsController.cs
+++ Pierre/Controllers/TreatsController.cs
@@ -27,6 +27,17 @@ namespace Pierre.Controllers
       List<Treat> model = _db.Treats.ToList();
       return View(model);
     }
+
+    public ActionResult Details(int id)
+    {
+        var thisTreat = _db.Treats
+            .Include(treat => treat.Flavors)
+            .ThenInclude(join => join.Flavor)
+            .FirstOrDefault(treat => treat.TreatId == id);
+        return View(thisTreat);
+    }
+
+     [Authorize] 
     public ActionResult Create()
     {
       ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
@@ -44,14 +55,6 @@ namespace Pierre.Controllers
         }
         _db.SaveChanges();
         return RedirectToAction("Index");
-    }
-    public ActionResult Details(int id)
-    {
-        var thisTreat = _db.Treats
-            .Include(treat => treat.Flavors)
-            .ThenInclude(join => join.Flavor)
-            .FirstOrDefault(treat => treat.TreatId == id);
-        return View(thisTreat);
     }
     public ActionResult Edit(int id)
     {
